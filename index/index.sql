27354dd664e843fb04dceacd233d9ce54fd1be0e
Isabel Araújo <isabelcalvesaraujo@hotmail.com>
2023-07-20 18:21:33 -0300

    att DB

--- index/index.sql
+++ index/index.sql
@@ -1,3 +1,63 @@
+--inserção de dados e queries
+use ecommerce
+
+show tables;
+
+-- Inserção de dados de exemplo
+INSERT INTO Client (idClient, Fname, Email) VALUES
+    (1, 'Empresa XYZ', 'empresa@xyz.com'),
+    (2, 'João Silva', 'joao@email.com'),
+    (3, 'Maria Santos', 'maria@email.com');
+
+INSERT INTO Location (idLocation, idClient, Rua, Numero, Cidade, Estado, CEP) VALUES
+    (1, 1, 'Rua A', '123', 'São Paulo', 'SP', '01234-567'),
+    (2, 2, 'Rua B', '456', 'Rio de Janeiro', 'RJ', '23456-789'),
+    (3, 3, 'Rua C', '789', 'Belo Horizonte', 'MG', '34567-890');
+
+INSERT INTO Payments (idPayment, typePayment) VALUES
+    (1, 'Cartão'),
+    (2, 'Boleto');
+
+INSERT INTO PaymentCash (idClient, idPayment) VALUES
+    (1, 1),
+    (1, 2),
+    (2, 1),
+    (3, 1),
+    (3, 3);
+
+INSERT INTO Seller (idSeller, SocialName, Email, contact) VALUES
+    (1, 'Pedro Oliveira', 'pedro@email.com', '(11) 98989-7878'),
+    (2, 'Ana Souza', 'ana@email.com', '(21) 97777-4545');
+
+INSERT INTO Supplier (idSupplier, SocialName, Email, contact) VALUES
+    (1, 'Fornecedor A', 'fornecedorA@email.com', '(11) 98989-7878'),
+    (2, 'Fornecedor B', 'fornecedorB@email.com', '(21) 97777-4545');
+
+INSERT INTO Product (idProduct, Pname, Category, Price, Estoque, idSupplier) VALUES
+    (1, 'Produto 1', 'Descrição do Produto 1', 50.00, 100, 1),
+    (2, 'Produto 2', 'Descrição do Produto 2', 75.00, 50, 2),
+    (3, 'Produto 3', 'Descrição do Produto 3', 30.00, 200, 1);
+
+INSERT INTO Orders (idOrders, idClient, idSeller, DateOrder) VALUES
+    (1, 1, 1, '2023-07-10'),
+    (2, 1, 2, '2023-07-12'),
+    (3, 2, 1, '2023-07-14'),
+    (4, 3, 2, '2023-07-15');
+
+INSERT INTO ProductOrder (idOrder, idProduct, Quantity) VALUES
+    (1, 1, 3),
+    (1, 2, 2),
+    (2, 3, 1),
+    (3, 1, 5),
+    (4, 3, 2);
+
+INSERT INTO Delivery (idDelivery, idOrder, deliveryStatus, Tracking) VALUES
+    (1, 1, 'Em trânsito', 'ABC123'),
+    (2, 2, 'Entregue', 'XYZ789'),
+    (3, 3, 'Aguardando envio', NULL),
+    (4, 4, 'Em trânsito', 'DEF456');
+
+
 --para alterar o valor de cada pedido
 
 SELECT PedidoID, SUM(Preco * Quantidade) AS ValorTotal FROM Pedido
--- index/mysql_index.sql
+++ index/mysql_index.sql
@@ -1,137 +1,129 @@
--- Criação das tabelas
-CREATE TABLE Cliente (
-    ClienteID INT PRIMARY KEY,
-    Nome VARCHAR(100),
-    Email VARCHAR(100),
-    Telefone VARCHAR(20),
-    TipoCliente ENUM('PJ', 'PF')
+-- criacao do banco de dados para o cenário de E-commerce
+create database ecommerce;
+use ecommerce;
+
+--criar tabela cliente
+
+create table clients(
+    idClient int auto_increment primary key,
+    Fname varchar(10),
+    Minit char(3),
+    Lname varchar(20),
+    email varchar(255) default 0,
+    CPF char(11) not null,
+    Address varchar(30),
+    constraint unique_cpf_client unique (CPF)
 );
 
-CREATE TABLE Endereco (
-    EnderecoID INT PRIMARY KEY,
-    ClienteID INT,
-    Rua VARCHAR(100),
-    Numero VARCHAR(10),
-    Cidade VARCHAR(100),
-    Estado VARCHAR(50),
-    CEP VARCHAR(10),
-    FOREIGN KEY (ClienteID) REFERENCES Cliente (ClienteID)
+desc clients;
+--criar tabela produto
+--size = dimensão do produto
+create table product(
+    idProduct int auto_increment primary key,
+    Pname varchar(10) not null,
+    classification_kids bool default false,
+    category enum('Eletrônico', 'Vestimenta', 'Brinquedos', 'Alimentos', 'Moveis') not null,
+    avaliacao float default 0,
+    price int auto_increment primary ket,
+    size varchar(10),
+    constraint unique_cpf_client unique (CPF)
+);
+--criar tabela pagamentos
+create table payments(
+    idclient int primary key,
+    idPayment int,
+    typePayment enum('Boleto', 'Cartão', 'Dois cartões'),
+    limitAvailable float,
+    primary key(idClient, id_payment)
 );
 
-CREATE TABLE FormaPagamento (
-    FormaPagamentoID INT PRIMARY KEY,
-    Descricao VARCHAR(100)
+--criar tabela pedido
+create table orders(
+    idOrders int auto_increment primary key,
+    idOrdersClient int,
+    ordersStatus enum('Cancelado', 'Confirmado', 'Em processamento') default 'Em processamento',
+    ordersDescription varchar(255),
+    sendValue float default 10,
+    paymentCash boolean default false,
+    constraint fk_orders_client foreign key (idOrdersClient) references clients(idClient)
+        on update cascade
 );
 
-CREATE TABLE ClienteFormaPagamento (
-    ClienteID INT,
-    FormaPagamentoID INT,
-    PRIMARY KEY (ClienteID, FormaPagamentoID),
-    FOREIGN KEY (ClienteID) REFERENCES Cliente (ClienteID),
-    FOREIGN KEY (FormaPagamentoID) REFERENCES FormaPagamento (FormaPagamentoID)
+--criar tabela estoque
+create table productsStorage(
+    idProdStorage int auto_increment primary key,
+    storageLocation varchar(255),
+    quantity int default 0,
+);
+--criar tabela fornecedor
+create table supplier(
+    idSupplier in auto_increment primary key,
+    SocialName varchar(255) not null,
+    email char(255) default 0,
+    CNPJ char(15) not null,
+    contact char(11) not null,
+    constraint unique_supplier unique(CNPJ),
+);
+--criar tabela vendedor
+create table seller(
+    idSeller in auto_increment primary key,
+    SocialName varchar(255) not null,
+    AbsName varchar(255),
+    Location char(255) not null,
+    email char(255) default 0,
+    CNPJ char(15),
+    CPF char(9),
+    contact char(11) not null,
+    constraint unique_seller unique(CNPJ),
+    constraint unique_seller unique(CPF)
 );
 
-CREATE TABLE Vendedor (
-    VendedorID INT PRIMARY KEY,
-    Nome VARCHAR(100),
-    Email VARCHAR(100),
-    Telefone VARCHAR(20)
+create table productSeller(
+    idPSeller int,
+    idPproduct int,
+    prodQuantity int default 1,
+    primary key (idPSeller, idPproduct),
+    constraint fk_product_seller foreign key(idpSeller) references seller(idSeller),
+    constraint fk_product_product foreign key (idPproduct) references product(idProduct)
+)
+
+create table productOrder(
+    idPOproduct int,
+    idPOorder int,
+    poQuantity int default 1,
+    poStatus enum ('Disponível', 'Sem estoque') default 'Disponível',
+    primary key (idPOproduct, idPOorder),
+    constraint fk_product_seller foreign key (idPOproduct) references product(idProduct),
+    constraint fk_product_product foreign key (idPOorder) references orders (idOrder)
 );
 
-CREATE TABLE Fornecedor (
-    FornecedorID INT PRIMARY KEY,
-    Nome VARCHAR(100),
-    Email VARCHAR(100),
-    Telefone VARCHAR(20)
+create table storageLocation(
+    idLproduct int,
+    idLstorage int,
+    location varchar(255) not null,
+    constraint fk_storage_location_product foreign key (idLproduct) references product(idProduct),
+    constraint fk_storage_location_storage foreign key (idLstorage) references orders(productStorage)
 );
 
-CREATE TABLE Produto (
-    ProdutoID INT PRIMARY KEY,
-    Nome VARCHAR(100),
-    Descricao VARCHAR(255),
-    Preco DECIMAL(10, 2),
-    Estoque INT,
-    FornecedorID INT,
-    FOREIGN KEY (FornecedorID) REFERENCES Fornecedor (FornecedorID)
+create table productSupplier(
+    idPsSupplier int,
+    idPsProduct int,
+    quantity int not null,
+    primary key (idPsSupplier, idPsProduct),
+    constraint fk_product_supplier_supplier foreign key (idPsSupplier) references supplier(idSupplier),
+    constraint fk_product_supplier_product foreign key (idPsProduct) references product(idProduct)
 );
 
-CREATE TABLE Pedido (
-    PedidoID INT PRIMARY KEY,
-    ClienteID INT,
-    VendedorID INT,
-    DataPedido DATE,
-    FOREIGN KEY (ClienteID) REFERENCES Cliente (ClienteID),
-    FOREIGN KEY (VendedorID) REFERENCES Vendedor (VendedorID)
-);
+create table delivery(
+    idDelivery int auto_increment primary key, 
+    idOrder int,
+    deliveryStatus int ('aguardando envio', 'em trânsito', 'entregue'),
+    tracking varchar(255) default null
+)
 
-CREATE TABLE ItemPedido (
-    PedidoID INT,
-    ProdutoID INT,
-    Quantidade INT,
-    PRIMARY KEY (PedidoID, ProdutoID),
-    FOREIGN KEY (PedidoID) REFERENCES Pedido (PedidoID),
-    FOREIGN KEY (ProdutoID) REFERENCES Produto (ProdutoID)
-);
+show tables;
+show databases;
 
-CREATE TABLE Entrega (
-    EntregaID INT PRIMARY KEY,
-    PedidoID INT,
-    Status VARCHAR(50),
-    CodigoRastreio VARCHAR(50),
-    FOREIGN KEY (PedidoID) REFERENCES Pedido (PedidoID)
-);
-
--- Inserção de dados de exemplo
-INSERT INTO Cliente (ClienteID, Nome, Email, Telefone, TipoCliente) VALUES
-    (1, 'Empresa XYZ', 'empresa@xyz.com', '(11) 99999-8888', 'PJ'),
-    (2, 'João Silva', 'joao@email.com', '(21) 98888-7777', 'PF'),
-    (3, 'Maria Santos', 'maria@email.com', '(31) 97777-6666', 'PF');
-
-INSERT INTO Endereco (EnderecoID, ClienteID, Rua, Numero, Cidade, Estado, CEP) VALUES
-    (1, 1, 'Rua A', '123', 'São Paulo', 'SP', '01234-567'),
-    (2, 2, 'Rua B', '456', 'Rio de Janeiro', 'RJ', '23456-789'),
-    (3, 3, 'Rua C', '789', 'Belo Horizonte', 'MG', '34567-890');
-
-INSERT INTO FormaPagamento (FormaPagamentoID, Descricao) VALUES
-    (1, 'Cartão de Crédito'),
-    (2, 'Boleto'),
-    (3, 'Transferência Bancária');
-
-INSERT INTO ClienteFormaPagamento (ClienteID, FormaPagamentoID) VALUES
-    (1, 1),
-    (1, 2),
-    (2, 1),
-    (3, 1),
-    (3, 3);
-
-INSERT INTO Vendedor (VendedorID, Nome, Email, Telefone) VALUES
-    (1, 'Pedro Oliveira', 'pedro@email.com', '(11) 98989-7878'),
-    (2, 'Ana Souza', 'ana@email.com', '(21) 97777-4545');
-
-INSERT INTO Fornecedor (FornecedorID, Nome, Email, Telefone) VALUES
-    (1, 'Fornecedor A', 'fornecedorA@email.com', '(11) 98989-7878'),
-    (2, 'Fornecedor B', 'fornecedorB@email.com', '(21) 97777-4545');
-
-INSERT INTO Produto (ProdutoID, Nome, Descricao, Preco, Estoque, FornecedorID) VALUES
-    (1, 'Produto 1', 'Descrição do Produto 1', 50.00, 100, 1),
-    (2, 'Produto 2', 'Descrição do Produto 2', 75.00, 50, 2),
-    (3, 'Produto 3', 'Descrição do Produto 3', 30.00, 200, 1);
-
-INSERT INTO Pedido (PedidoID, ClienteID, VendedorID, DataPedido) VALUES
-    (1, 1, 1, '2023-07-10'),
-    (2, 1, 2, '2023-07-12'),
-    (3, 2, 1, '2023-07-14'),
-    (4, 3, 2, '2023-07-15');
-
-INSERT INTO ItemPedido (PedidoID, ProdutoID, Quantidade) VALUES
-    (1, 1, 3),
-    (1, 2, 2),
-    (2, 3, 1),
-    (3, 1, 5),
-    (4, 3, 2);
-
-INSERT INTO Entrega (EntregaID, PedidoID, Status, CodigoRastreio) VALUES
-    (1, 1, 'Em trânsito', 'ABC123'),
-    (2, 2, 'Entregue', 'XYZ789'),
-    (3, 3, 'Aguardando envio', NULL),
-    (4, 4, 'Em trânsito', 'DEF456');
+use information_schema;
+show tables;
+desc referential_constraints;
